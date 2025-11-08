b0003faa6e97adfe71ef0a63a736a4505ce4c767
firfaronde <223457997+firfaronde@users.noreply.github.com>
2025-11-09 04:40:30 +0500

    fr

--- make_sponsors.sql
+++ make_sponsors.sql
@@ -8,11 +8,11 @@ CREATE TABLE sponsors (
 CREATE TABLE sponsors_tiers (
     id SERIAL PRIMARY KEY,
     sponsor_id INT NOT NULL UNIQUE,
-    tier INT NOT NULL,
+    tier INT NOT NULL DEFAULT 1,
     oocColor TEXT NOT NULL,
     havePriorityJoin BOOLEAN NOT NULL DEFAULT FALSE,
     extraSlots INT NOT NULL DEFAULT 0,
     allowedMarkings TEXT[] NOT NULL DEFAULT '{}',
-    ghostTheme TEXT NOT NULL DEFAULT 'light',
+    ghostTheme TEXT NOT NULL DEFAULT 'WisdomGhost',
     FOREIGN KEY (sponsor_id) REFERENCES sponsors(id) ON DELETE CASCADE
 );
